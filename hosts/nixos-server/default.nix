22a3290742684441aa0c2b5a7a84960f819e8ea3
ri <vanish-relay@pm.me>
2026-09-24 12:58:58 +0300

    Add Ghostty terminfo to nixos-server

--- hosts/nixos-server/default.nix
+++ hosts/nixos-server/default.nix
@@ -25,7 +25,7 @@
   boot.loader.timeout = 5;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  environment.systemPackages = with pkgs; [ git vim pam_u2f ];
+  environment.systemPackages = with pkgs; [ git vim pam_u2f ghostty.terminfo ];
   users.users.ri = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
