0d444bf871f773fb7fb59b1bddf7c347592df091
ri <vanish-relay@pm.me>
2026-09-25 19:58:15 +0300

    Add router SSH alias and key permissions

--- common/modules/ssh.nix
+++ common/modules/ssh.nix
@@ -15,6 +15,12 @@
         IdentityFile = "~/.ssh/r1";
         IdentitiesOnly = true;
       };
+      "r2" = {
+        HostName = "r2.local";
+        User = "root";
+        IdentityFile = "~/.ssh/r2";
+        IdentitiesOnly = true;
+      };
       "nixos-server" = {
         HostName = "nixos-server.local";
         HostKeyAlias = "nixos-server.local";
@@ -37,7 +43,7 @@
     if [ -d "$HOME/.ssh" ]; then
       run chmod 700 "$HOME/.ssh"
     fi
-    for key in "$HOME/.ssh/nixos-server" "$HOME/.ssh/r1"; do
+    for key in "$HOME/.ssh/nixos-server" "$HOME/.ssh/r1" "$HOME/.ssh/r2"; do
       if [ -f "$key" ]; then
         run chmod 600 "$key"
       fi
